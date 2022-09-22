13769374e9ab11c120b60a17788dce1689afc81b
Young Hwang <ykcul02@gmail.com>
2022-09-23 00:24:21 +0900

    07.00 - 어노테이션의 변화

---
+++ src/main/java/io/younghwang/java8/Chicken.java
@@ -0,0 +1,11 @@
+package io.younghwang.java8;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_PARAMETER) // generic 을 쓸때 타입 부분에 사용
+public @interface Chicken {
+}
