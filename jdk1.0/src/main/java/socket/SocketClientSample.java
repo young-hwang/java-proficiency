2e512717b85f92bd4d70f071283ffbebdd51425b
Young Hwang <ykcul02@gmail.com>
2022-10-29 18:02:18 +0900

    add: server send message

--- jdk1.0/src/main/java/socket/SocketClientSample.java
+++ jdk1.0/src/main/java/socket/SocketClientSample.java
@@ -1,7 +1,10 @@
 package socket;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Date;
@@ -9,7 +12,7 @@ import java.util.Date;
 public class SocketClientSample {
     public static void main(String[] args) {
         SocketClientSample sample = new SocketClientSample();
-        sample.sendSocketSample();
+        sample.sendAndReceiveSocketData();
     }
 
     private void sendSocketSample() {
@@ -34,6 +37,34 @@ public class SocketClientSample {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    private void sendAndReceiveSocketData() {
+        Socket socket = null;
+        try {
+            System.out.println("Client: Connecting..");
+            socket = new Socket("127.0.0.1", 9999);
+            System.out.println("Client: Connect status=" + socket.isConnected());
+            Thread.sleep(1000);
+            InputStream outputStream = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(outputStream));
+            String data = null;
+            StringBuilder receivedData = new StringBuilder();
+            while ((data = reader.readLine()) != null) {
+                receivedData.append(data);
+            }
+            System.out.println(receivedData);
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             if (socket != null) {
--- jdk1.0/src/main/java/socket/SocketServerSample.java
+++ jdk1.0/src/main/java/socket/SocketServerSample.java
@@ -1,16 +1,19 @@
 package socket;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServerSample {
     public static void main(String[] args) {
         SocketServerSample server = new SocketServerSample();
-        server.startServer();
+        server.startReplyServer();
     }
 
     private void startServer() {
@@ -39,6 +42,36 @@ public class SocketServerSample {
                  }
                  System.out.println("------------------");
              }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void startReplyServer() {
+        ServerSocket serverSocket = null;
+        Socket client = null;
+        try {
+            serverSocket = new ServerSocket(9999);
+            while(true) {
+                System.out.println("Server: Waiting for request.");
+                client = serverSocket.accept();
+                System.out.println("Server: Accepted");
+                OutputStream stream = client.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stream));
+                writer.write("OK");
+                writer.close();
+                stream.close();
+                client.close();
+                System.out.println("------------------");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
