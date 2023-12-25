962a45fb2734b401de5bde9d39b3bc4455c3b3f4
young-hwang <ykcul02@gmail.com>
2023-12-25 15:17:25 +0900

    add: generate fibonacci
    add: generate fibonacci

--- src/main/java/me/stream/InfiniteStream.java
+++ src/main/java/me/stream/InfiniteStream.java
@@ -1,5 +1,7 @@
 package me.stream;
 
+import java.util.function.IntSupplier;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class InfiniteStream {
@@ -21,14 +23,34 @@ public class InfiniteStream {
 
         // stream short circuit
         Stream.iterate(0, n -> n + 2)
-                .takeWhile(n -> n < 100)
+                .takeWhile(n -> n < 100)    // java 9
                 .forEach(System.out::println);
 
         System.out.println("=====================================");
 
         // generate 메서드 사용
-        java.util.stream.Stream.generate(Math::random)
+        Stream.generate(Math::random)
                 .limit(5)
                 .forEach(System.out::println);
+
+        System.out.println("=====================================");
+
+        // generate fibonacci
+
+        IntSupplier intSupplier = new IntSupplier() {
+            private int previous = 0;
+            private int current = 1;
+
+            @Override
+            public int getAsInt() {
+                int oldPrevious = this.previous;
+                int nextValue = this.previous + this.current;
+                this.previous = this.current;
+                this.current = nextValue;
+                return oldPrevious;
+            }
+        };
+
+        IntStream.generate(intSupplier).limit(10).forEach(System.out::println);
     }
 }
