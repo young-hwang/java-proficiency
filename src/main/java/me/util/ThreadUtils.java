e9061b92224764d80a8bae40bd08b828e74fb5b9
young-hwang <ykcul02@gmail.com>
2024-07-26 01:47:27 +0900

    add: thread util

---
+++ src/main/java/me/util/ThreadUtils.java
@@ -0,0 +1,16 @@
+package me.util;
+
+import static me.util.MyLogger.log;
+
+public abstract class ThreadUtils {
+
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            log("Interrupt 발생" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+}
