a956278a22db76a5aeafc57e859fd24e39487e0d
young-hwang <ykcul02@gmail.com>
2024-07-31 23:22:47 +0900

    add: thread example print

---
+++ src/main/java/me/thread/interrupt/MyPrinter1.java
@@ -0,0 +1,50 @@
+package me.thread.interrupt;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static me.util.MyLogger.log;
+import static me.util.ThreadUtils.sleep;
+
+public class MyPrinter1 {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer);
+        printerThread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            log("프린터할 문서를 입력하세요. 종료 (q): ");
+            String input = scanner.nextLine();
+            if ("q".equals(input)) {
+                printer.work = false;
+                break;
+            }
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+        volatile boolean work = true;
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            while(work) {
+                if (jobQueue.isEmpty()) {
+                    continue;
+                }
+
+                String job = jobQueue.poll();
+                log("출력 시작: " + job + ", 대기 문서: " + jobQueue);
+                sleep(3000);
+                log("출력 완료");
+            }
+        }
+
+        public void addJob(String job) {
+            this.jobQueue.add(job);
+        }
+    }
+}
