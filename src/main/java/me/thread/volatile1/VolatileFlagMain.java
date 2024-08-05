6ee7cb787018d10d094d80512fb62f498b7d2973
young-hwang <ykcul02@gmail.com>
2024-08-05 22:19:35 +0900

    add: memory visibility 문제 정의

--- src/main/java/me/thread/volatile1/VolatileFlagMain.java
+++ src/main/java/me/thread/volatile1/VolatileFlagMain.java
@@ -20,6 +20,8 @@ public class VolatileFlagMain {
         // 메모리 가시성(Memory Visibility) 문제
         // 멀티스레드 환경에서 한 스레드가 변경한 값이 다른 스레드에서 언제 보이는지에 대한 문제
         // 이름 그대로 메모리에 변경한 값이 보이는가? 보이지 않는가? 의 문제
+        // 힙 영역 메모리 사용이 메인 메모리로 생각하지만 실제론 코어의 캐시 메모리에서 불러온다.
+        // 따라서 각 스레드 마다 캐시 메모리에 값이 각각 존재하여 한 스레드가 변경한 값이 메인 메모리에 반영이 될 수도 되지 않을 수도있다.
         public boolean runFlag = true;
 //        public volatile boolean runFlag = true;
 
