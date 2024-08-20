345468d5d46281f6ef7d51d4a1df7550eff7bd2c
young-hwang <ykcul02@gmail.com>
2024-08-20 10:09:02 +0900

    fix: modify builder process

--- annotation/annotaion-client/build.gradle.kts
+++ annotation/annotaion-client/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":annotation:annotation-processor"))
+    annotationProcessor(project(":annotation:annotation-processor"))
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
