64e4e1f478ba433e0067edab7013526b83573379
young-hwang <ykcul02@gmail.com>
2024-09-19 23:34:16 +0900

    add: Producer & Consumer Problem v6_3

---
+++ src/main/java/me/thread/bounded/BoundedQueueV6_3.java
@@ -0,0 +1,41 @@
+package me.thread.bounded;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import static me.util.MyLogger.log;
+
+public class BoundedQueueV6_3 implements BoundedQueue {
+    private final BlockingQueue<String> queue;
+
+    public BoundedQueueV6_3(int max) {
+        this.queue = new ArrayBlockingQueue<>(max);
+    }
+
+    @Override
+    public void put(String data) {
+        boolean result = false;
+        try {
+            this.queue.offer(data, 1, TimeUnit.NANOSECONDS);
+            log("저장 시도 결과 = " + result);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String take() {
+        try {
+            // 대기 시간 설정
+            return this.queue.poll(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
+    }
+}
