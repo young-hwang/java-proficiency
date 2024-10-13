850adc88fcdda9924e9fd091855bbe7a7f764f09
young-hwang <ykcul02@gmail.com>
2024-10-13 22:12:38 +0900

    add: 스트림 시작 1

---
+++ src/main/java/me/file/stream/StreamStartMainV1.java
@@ -0,0 +1,22 @@
+package me.file.stream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class StreamStartMainV1 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream("temp/hello.dat");
+        fos.write(65);
+        fos.write(66);
+        fos.write(67);
+        fos.close();
+
+        FileInputStream fis = new FileInputStream("temp/hello.dat");
+        System.out.println(fis.read());
+        System.out.println(fis.read());
+        System.out.println(fis.read());
+        System.out.println(fis.read());
+        fis.close();
+    }
+}
---
+++ src/main/java/me/file/stream/StreamStartMainV2.java
@@ -0,0 +1,22 @@
+package me.file.stream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class StreamStartMainV2 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream("temp/hello.dat");
+        fos.write(65);
+        fos.write(66);
+        fos.write(67);
+        fos.close();
+
+        FileInputStream fis = new FileInputStream("temp/hello.dat");
+        int data;
+        while((data = fis.read()) != -1) {
+            System.out.println(data);
+        }
+        fis.close();
+    }
+}
