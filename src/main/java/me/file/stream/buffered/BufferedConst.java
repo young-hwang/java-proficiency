a8548c8a1d53dd527703d8901db917b43bac70dd
young-hwang <ykcul02@gmail.com>
2024-10-14 22:26:52 +0900

    add: 파일 입출력과 성능 최적화1 - 하나씩 쓰기

---
+++ src/main/java/me/file/stream/buffered/BufferedConst.java
@@ -0,0 +1,7 @@
+package me.file.stream.buffered;
+
+public class BufferedConst {
+    public static final String FILE_NAME = "temp/buffered.dat";
+    public static final int FILE_SIZE = 1024 * 1024 * 10; // 10MB
+    public static final int DEFAULT_BUFFER_SIZE = 8192; // 8kb
+}
---
+++ src/main/java/me/file/stream/buffered/CreateFileV1.java
@@ -0,0 +1,22 @@
+package me.file.stream.buffered;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static me.file.stream.buffered.BufferedConst.FILE_NAME;
+import static me.file.stream.buffered.BufferedConst.FILE_SIZE;
+
+public class CreateFileV1 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream(FILE_NAME);
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < FILE_SIZE; i++) {
+            fos.write(1);
+        }
+        fos.close();
+        long endTime = System.currentTimeMillis();
+        System.out.println("File created: " + FILE_NAME);
+        System.out.println("File size: " + FILE_SIZE / 1024 / 1024 + "MB");
+        System.out.println("Time take: " + (endTime - startTime) + "ms") ;
+    }
+}
