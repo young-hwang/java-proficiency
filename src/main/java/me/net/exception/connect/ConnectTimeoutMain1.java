b4e365227e7c14d9c4c397e33f4ab774daeaaff0
young-hwang <ykcul02@gmail.com>
2024-11-19 00:39:42 +0900

    add: 네트워크 예외2 - 타임아웃

---
+++ src/main/java/me/net/exception/connect/ConnectTimeoutMain1.java
@@ -0,0 +1,21 @@
+package me.net.exception.connect;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+
+// Windows: 약 21초
+// Linux: 약 75초 에서 180초 사이
+// Mac: 75초
+public class ConnectTimeoutMain1 {
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        try {
+            Socket socket = new Socket("192.168.1.250", 45678);
+        } catch (ConnectException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("end: " + (end - start));
+    }
+}
---
+++ src/main/java/me/net/exception/connect/ConnectTimeoutMain2.java
@@ -0,0 +1,24 @@
+package me.net.exception.connect;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+// Windows: 약 21초
+// Linux: 약 75초 에서 180초 사이
+// Mac: 75초
+public class ConnectTimeoutMain2 {
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress("192.168.1.250", 45678), 2000);
+        } catch (SocketTimeoutException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("end: " + (end - start));
+    }
+}
