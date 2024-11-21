705f779a7fb3cfe0f530c946dd0d42cd5542ed75
young-hwang <ykcul02@gmail.com>
2024-11-21 23:11:38 +0900

    add: 네트워크 예외4 - 강제 종료

---
+++ src/main/java/me/net/exception/close/reset/ResetCloseServer.java
@@ -0,0 +1,18 @@
+package me.net.exception.close.reset;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static me.util.MyLogger.log;
+
+public class ResetCloseServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(12345);
+        Socket socket = serverSocket.accept();
+        log("Connected Socket: " + socket);
+        socket.close();
+        serverSocket.close();
+        log("Closed Socket: " + socket);
+    }
+}
