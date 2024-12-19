b1a7e5388d7fd733d0964a03351289a11b6ff8c0
young-hwang <ykcul02@gmail.com>
2024-12-19 22:58:44 +0900

    add: 클래스와 메타데이터

---
+++ src/main/java/me/reflection/BasicV1.java
@@ -0,0 +1,24 @@
+package me.reflection;
+
+import me.reflection.data.BasicData;
+
+public class BasicV1 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        // 클래스 메타데이터 조회 방법 3가지
+
+        // 1. 클래스에서 찾기
+        Class<BasicData> basicDataClass1 = BasicData.class;
+        System.out.println("basicDataClass1: " + basicDataClass1);
+
+        // 2. 인스턴스에서 찾기
+        BasicData basicInstance = new BasicData();
+        // 실제 인스턴스가 자식 인스턴스 일수도 있으므로
+        Class<? extends BasicData> basicDataClass2 = basicInstance.getClass();
+        System.out.println("basicDataClass2: " + basicDataClass2);
+
+        // 3. 문자로 찾기
+        String className = "me.reflection.data.BasicData";
+        Class<?> basicDataClass3 = Class.forName(className);
+        System.out.println("basicDataClass3: " + basicDataClass3);
+    }
+}
---
+++ src/main/java/me/reflection/BasicV2.java
@@ -0,0 +1,27 @@
+package me.reflection;
+
+import me.reflection.data.BasicData;
+
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+public class BasicV2 {
+    public static void main(String[] args) {
+        Class<BasicData> basicDataClass = BasicData.class;
+        System.out.println("basicDataClass.getName() = " + basicDataClass.getName());
+        System.out.println("basicDataClass.getSimpleName() = " + basicDataClass.getSimpleName());
+        System.out.println("basicDataClass.getPackageName() = " + basicDataClass.getPackageName());
+
+        System.out.println("basicDataClass.getSuperclass() = " + basicDataClass.getSuperclass());
+        System.out.println("basicDataClass.getInterfaces() = " + Arrays.toString(basicDataClass.getInterfaces()));
+
+        System.out.println("basicDataClass.isInterface() = " + basicDataClass.isInterface());
+        System.out.println("basicDataClass.isEnum() = " + basicDataClass.isEnum());
+        System.out.println("basicDataClass.isAnnotations() = " + basicDataClass.isAnnotation());
+
+        int modifiers = basicDataClass.getModifiers();
+        System.out.println("basicDataClass.getModifiers() = " + modifiers);
+        System.out.println("isPublic() = " + Modifier.isPublic(modifiers));
+        System.out.println("Modifier.toString() = " + Modifier.toString(modifiers));
+    }
+}
