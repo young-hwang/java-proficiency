8a02e4877cde3cf0f51916f5dddc2f19eaa2a0ee
young-hwang <younghwang@micube.co.kr>
2025-03-05 18:53:28 +0900

    add: week of month

--- src/test/java/datetime/WeekOfMonthTest.java
+++ src/test/java/datetime/WeekOfMonthTest.java
@@ -2,17 +2,18 @@ package datetime;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.WeekFields;
 import java.util.Locale;
 
 public class WeekOfMonthTest {
 
     public static void main(String[] args) {
-        System.out.println(DayOfWeek.THURSDAY.getValue());
         // 특정 날짜 설정 (예: 2023년 11월 15일)
-        LocalDate date = LocalDate.of(2025, 3, 1);
-
-        LocalDate yesterday = date.plusDays(-1);
+        LocalDate date = LocalDate.of(2025, 3, 2);
+        System.out.println(DayOfWeek.THURSDAY.getValue());
+        System.out.println(date.getDayOfMonth());
+        LocalDate preMonthLastDate = date.plusDays(-1 * (date.getDayOfMonth()));
 
         // 현재 날짜를 사용하려면 아래 코드 사용
         // LocalDate date = LocalDate.now();
@@ -22,18 +23,17 @@ public class WeekOfMonthTest {
 
         // 해당 월의 몇 번째 주인지 계산
         int weekOfMonth = date.get(weekFields.weekOfMonth());
-        int yesterdayWeekOfMonth = yesterday.get(weekFields.weekOfMonth());
+        int yesterdayWeekOfMonth = preMonthLastDate.get(weekFields.weekOfMonth());
 
         System.out.println("날짜: " + date);
         System.out.println("해당 월의 " + weekOfMonth + "번째 주입니다.");
 
-        System.out.println("날짜: " + yesterday);
+        System.out.println("날짜: " + preMonthLastDate);
         System.out.println("해당 월의 " + yesterdayWeekOfMonth + "번째 주입니다.");
 
-
         // 추가 정보 출력
-        System.out.println("월: " + date.getMonthValue());
-        System.out.println("일: " + date.getDayOfMonth());
-        System.out.println("요일: " + date.getDayOfWeek());
+//        System.out.println("월: " + date.getMonthValue());
+//        System.out.println("일: " + date.getDayOfMonth());
+//        System.out.println("요일: " + date.getDayOfWeek());
     }
 }
